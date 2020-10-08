b7b490f5c60988934e9d45f5e14741dc05969507
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-10-08 07:36:01 -0300

    Começando programa para leitura de informações de um produto

---
+++ CursoJavaClasses/src/aplicacao/ProgramaProduto.java
@@ -0,0 +1,31 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Produto;
+
+public class ProgramaProduto {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Produto produto = new Produto();
+		System.out.println("Entre com os dados do produto: ");
+
+		System.out.println("Nome: ");
+		produto.nome = sc.nextLine();
+
+		System.out.println("Preço: ");
+		produto.preco = sc.nextDouble();
+
+		System.out.println("Quantidade em estoque: ");
+		produto.quantidade = sc.nextInt();
+
+		sc.close();
+
+	}
+
+}
