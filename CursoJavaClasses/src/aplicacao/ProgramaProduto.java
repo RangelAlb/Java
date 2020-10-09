1ffdb030a6163b78d2b3bc1f31b90245f916201f
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-10-09 07:13:43 -0300

    Finalizando programa

--- CursoJavaClasses/src/aplicacao/ProgramaProduto.java
+++ CursoJavaClasses/src/aplicacao/ProgramaProduto.java
@@ -24,8 +24,24 @@ public class ProgramaProduto {
 		System.out.println("Quantidade em estoque: ");
 		produto.quantidade = sc.nextInt();
 		
-		System.out.println(produto.toString());
-
+		System.out.println("Dados do produto: " + produto);
+		
+		System.out.println();
+		System.out.println("Entre com o número de produtos para ser adicionados no estoque: ");
+		int quantidade = sc.nextInt();
+		produto.adicionarProduto(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		
+		System.out.println();
+		System.out.println("Entre com o número de produtos para ser removidos no estoque: ");
+	    quantidade = sc.nextInt();
+		produto.removerProduto(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		
 		sc.close();
 
 	}
