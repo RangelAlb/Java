e0b41e1847256d9ccc185a6c3d557d3ecd8fa88f
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-11-23 07:03:24 -0300

    Herança

---
+++ CursoJavaSecao14/src/aplicacao/Heranca.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+import entidades.ContaDeNegocios;
+
+public class Heranca {
+
+	public static void main(String[] args) {
+	
+		ContaDeNegocios conta = new ContaDeNegocios();
+		
+		conta.getSaldo();
+		
+		
+	}
+
+}
---
+++ CursoJavaSecao14/src/entidades/ContaDeNegocios.java
@@ -0,0 +1,30 @@
+package entidades;
+
+public class ContaDeNegocios extends Conta {
+
+	private Double limiteDeEmprestimo;	
+
+	public ContaDeNegocios() {
+		super();
+	}
+
+	public ContaDeNegocios(Integer numero, String titular, Double saldo, Double limiteDeEmprestimo) {
+		super(numero, titular, saldo);
+		this.setLimiteDeEmprestimo(limiteDeEmprestimo);
+	}
+
+	public Double getLimiteDeEmprestimo() {
+		return limiteDeEmprestimo;
+	}
+
+	public void setLimiteDeEmprestimo(Double limiteDeEmprestimo) {
+		this.limiteDeEmprestimo = limiteDeEmprestimo;
+	}
+
+	public void emprestimo(double valor) {
+		if (valor <= limiteDeEmprestimo){
+			saldo += valor - 10.0;
+		}
+	}
+
+}
