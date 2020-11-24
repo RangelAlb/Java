ff77c7e2040fd85ab0ba2c3dcaec265d88efb7b0
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2020-11-24 07:29:29 -0300

    UpCasting & DownCasting

--- CursoJavaSecao14/src/aplicacao/Heranca.java
+++ CursoJavaSecao14/src/aplicacao/Heranca.java
@@ -1,15 +1,44 @@
 package aplicacao;
 
+import entidades.Conta;
 import entidades.ContaDeNegocios;
+import entidades.ContaPoupanca;
 
 public class Heranca {
 
 	public static void main(String[] args) {
 	
-		ContaDeNegocios conta = new ContaDeNegocios();
+     Conta acc = new Conta(1001, "Alex", 0.0);
+     
+     ContaDeNegocios bacc = new ContaDeNegocios(1002, "Maria", 0.0, 500.0);
 		
-		conta.getSaldo();
-		
+     
+     // UPCASTING
+     
+     Conta acc1 = bacc;
+     Conta acc2 = new ContaDeNegocios(1003, "Bob", 0.0, 200.0);
+     Conta acc3 = new ContaPoupanca(1004,"Anna", 0.0, 0.1);
+     
+     // DOWNCASTING
+     
+     ContaDeNegocios acc4 = (ContaDeNegocios)acc2;
+     acc4.emprestimo(100.0);
+     
+     // ContaDeNegocios acc5 = (ContaDeNegocios)acc3;
+     
+     if (acc3 instanceof ContaDeNegocios) {
+      ContaDeNegocios acc5 = (ContaDeNegocios)acc3;
+      acc5.depositar(200);
+      System.out.println("Empréstimo!");
+      
+     }
+     
+     if (acc3 instanceof ContaPoupanca) {
+    	 ContaPoupanca acc5 = (ContaPoupanca)acc3;
+    	 acc5.atualizarSaldo();
+    	 System.out.println("Atualizado!");
+     }
+     
 		
 	}
 
