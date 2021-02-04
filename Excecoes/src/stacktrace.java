52397af0f2febcd45beb9e9d7396a70026edbbfa
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2021-02-04 07:04:23 -0300

    stack trace

---
+++ Excecoes/src/stacktrace.java
@@ -0,0 +1,43 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class stacktrace {
+
+	public static void main(String[] args) {
+
+		method1();
+
+		System.out.println("Fim do programa");
+
+	}
+
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Posição Inválida!");
+			e.printStackTrace();
+		} catch (InputMismatchException e) {
+			System.out.println("Input Inválido!");
+		}
+
+		sc.close();
+		System.out.println("***METHOD2 END***");
+
+	}
+
+}
