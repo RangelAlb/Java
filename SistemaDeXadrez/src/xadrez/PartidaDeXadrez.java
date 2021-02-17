a241e56c901bd49ac47561066f8edd096312d324
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2021-02-17 06:17:47 -0300

    Criando peças Rei e Torre e colocando no tabuleiro

--- SistemaDeXadrez/src/xadrez/PartidaDeXadrez.java
+++ SistemaDeXadrez/src/xadrez/PartidaDeXadrez.java
@@ -1,6 +1,9 @@
 package xadrez;
 
+import jogodetabuleiro.Posicao;
 import jogodetabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaDeXadrez {
 
@@ -8,6 +11,7 @@ public class PartidaDeXadrez {
 	
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		configuracaoInicial();
 	}
 	
 	public PecaDeXadrez[][] getPecas(){
@@ -19,5 +23,11 @@ public class PartidaDeXadrez {
 		}
 		
 		return mat;
+	}
+	
+	private void configuracaoInicial() {
+		tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.WHITE), new Posicao(2, 1));
+		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BLACK), new Posicao(0, 4));
+		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.WHITE), new Posicao(7, 4));
 	}
 }
