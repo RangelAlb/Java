307fa8712a7e514247c84faaf1ab49d8ca00c08f
RangelAlb <rangel_albuquerque_2012@hotmail.com>
2021-02-18 07:07:27 -0300

    Criando XadrezExpection e XadrezPosicao

--- SistemaDeXadrez/src/xadrez/PartidaDeXadrez.java
+++ SistemaDeXadrez/src/xadrez/PartidaDeXadrez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import jogodetabuleiro.Posicao;
 import jogodetabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -8,27 +7,32 @@ import xadrez.pecas.Torre;
 public class PartidaDeXadrez {
 
 	private Tabuleiro tabuleiro;
-	
+
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
 		configuracaoInicial();
 	}
-	
-	public PecaDeXadrez[][] getPecas(){
+
+	public PecaDeXadrez[][] getPecas() {
 		PecaDeXadrez[][] mat = new PecaDeXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for (int i = 0; i<tabuleiro.getLinhas(); i++) {
-			for (int j = 0; j<tabuleiro.getColunas(); j++) {
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
 				mat[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j);
 			}
 		}
-		
+
 		return mat;
 	}
-	
+
+	private void colocarNovaPeca(char coluna, int linha, PecaDeXadrez peca) {
+		tabuleiro.colocarPeca(peca, new XadrezPosicao(coluna, linha).toPosicao());
+
+	}
+
 	private void configuracaoInicial() {
-		tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.WHITE), new Posicao(2, 1));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BLACK), new Posicao(0, 4));
-		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.WHITE), new Posicao(7, 4));
-		
+		colocarNovaPeca('b', 6, new Torre(tabuleiro, Cor.WHITE));
+		colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.BLACK));
+		colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.WHITE));
+
 	}
 }
